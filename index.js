afcf4f1d5773de5633706d9488f9ecede6d1e6e1
Gosha Evstigneev <evsti98@list.ru>
2025-03-05 17:10:50 +0300

    Добавил классы

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const b = "Привет задание 1"
 
 console.log("Привет мир")
 let mimi = 15
+
+class Person {
+    constructor(name, age, happiness) {
+        this.name = name;
+        this.age = age;
+        this.happiness = happiness;
+    }
+}
+
+let alex = new Person("Alex", 20, true);
+let bob = new Person("Bob", 30, false);
+console.log(alex.name);
+console.log(bob.name);
